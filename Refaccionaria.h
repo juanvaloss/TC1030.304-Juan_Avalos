97f3a52c89239bd9e90a70cc20f3e267299d7620
juanvalos <111295746+juanvalos@users.noreply.github.com>
2023-06-15 01:37:32 -0600

    Update Refaccionaria.h

--- Refaccionaria.h
+++ Refaccionaria.h
@@ -7,24 +7,31 @@ using namespace std;
 
 #include "Autoparte.h"
 
+// Creamos la clase Refaccionaria
 class Refaccionaria {
 
+// Declaramos y desarrollamos los atributos de la clase
     private:
-        vector<Autoparte*> inventario;
+    // Como unico atributo será un vector de tipo Autoparte, este se llamara inventario ya que es la funcion que tomara ese atributo
+        vector <Autoparte*> inventario;
 
     public:
-        Refaccionaria() {}
+    // Creamos un constructor vacio que va almacenar la informacion que se produzca en el main
+        Refaccionaria () {}
 
+    // Declaramos el getter 
         vector<Autoparte*> get_autopartes() {
             return inventario;
         }     
 
+    // Declaramos el metodo desplegarInventario    
         void desplegarInventario();
+    // Declaramos el metodo agregarAutoparte
         void agregarAutoparte(Autoparte* autop);
-
 };
 
-void Refaccionaria:: desplegarInventario() {          
+// Desarrollamos el metodo desplegarInventario, este servira para desplegar toda la informacion de las piezas existentes dentro del inventario de la refaccionaria
+void Refaccionaria :: desplegarInventario() {          
 
             cout << " " << endl ;
             cout << "Inventario de la Refaccionaria:" << endl ;
@@ -35,10 +42,12 @@ void Refaccionaria:: desplegarInventario() {
                 cout << " " << endl;
             }
 
-        } 
+} 
 
-void Refaccionaria::agregarAutoparte(Autoparte* autop) {  
+// Desarrollamos el metodo agregarAutoparte, este nos servira para poder agregar objetos de tipo Autoparte al vector inventario. 
+void Refaccionaria :: agregarAutoparte(Autoparte* autop) {  
     inventario.push_back(autop);
 }
 
+#endif
 #endif
